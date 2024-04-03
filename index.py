bbd2ca9d9efe06004289795702dc6482e768919e
AdiNik07 <vu5f2122007@pvppcoe.ac.in>
2024-04-04 00:54:25 +0530

    initial commit

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
+import g4f
+import time
 
 app=Flask(__name__)
 
@@ -6,6 +8,21 @@ app=Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/generate/<query>")
+def generateResponse(query):
+    print(query)
+    
+    response = g4f.ChatCompletion.create(
+        model="gpt-4-32k-0613",
+        provider=g4f.Provider.Bing,
+
+        messages=[{"role":"user","content":query}],
+        stream=False,
+    )
+
+    print("Response: \n",response)
+    return response  
+
 
 if __name__ == "__main__":
-    app.run()#(debug=False,host='0.0.0.0')
+    app.run(debug=False,host='0.0.0.0')
